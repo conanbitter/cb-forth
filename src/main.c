f03f9c932a70d2f921a9fe4ad0f9ff766b0cadc2
Conan Bitter <conan.bitter@gmail.com>
2026-06-29 21:24:50 +0300

    Implement stack functions. Add skeleton for core codes

--- src/main.c
+++ src/main.c
@@ -4,31 +4,64 @@
 typedef intptr_t Cell;
 #define CELL_WIDTH (sizeof(Cell))
 typedef void CodeFunc(Cell* data);
+#define STACK_SIZE (32)
+#define RSTACK_SIZE (16)
 
 Cell* code_ptr = NULL;
+Cell stack[STACK_SIZE];
+Cell* stack_ptr = &stack;
+Cell rstack[RSTACK_SIZE];
+Cell* rstack_ptr = &stack;
 
 inline void next() {
     code_ptr++;
 }
 
+void spush(Cell data) {
+    *stack_ptr = data;
+    stack_ptr++;
+}
+
+Cell spop() {
+    Cell data = *stack_ptr;
+    stack_ptr--;
+    return data;
+}
+
 void rpush(Cell data) {
+    *rstack_ptr = data;
+    rstack_ptr++;
 }
 
 Cell rpop() {
-    return 0;
+    Cell data = *rstack_ptr;
+    rstack_ptr--;
+    return data;
 }
 
-void c_docol(Cell* data) {
+void code_docol(Cell* data) {
     rpush((Cell)code_ptr);
     code_ptr = data + 1;
 }
 
-void c_exit(Cell* data) {
+void code_exit(Cell* data) {
     code_ptr = (Cell*)rpop();
 }
 
+void code_branch(Cell* data) {
+
+}
+
+void code_zbranch(Cell* data) {
+
+}
+
+void code_lit(Cell* data) {
+
+}
+
 const Cell static_dict[] = {
-    (Cell)&c_docol
+    (Cell)&code_docol
 };
 
 int main() {

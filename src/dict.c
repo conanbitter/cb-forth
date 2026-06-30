55f1fb749925d1c026956a5dc891edc5737cadee
Conan Bitter <conan.bitter@gmail.com>
2026-06-30 21:00:35 +0300

    Prepare for dictionary generation

--- src/dict.c
+++ src/dict.c
@@ -1,11 +1,12 @@
 #include "common.h"
 
-static const Cell sys_dict[5] = {
+static const Cell sys_dict[6] = {
     1,
     2,
     (Cell)&code_docol,
     3,
     (Cell)(sys_dict + 2),
+    0xABCDEF12,
 };
 
 Cell* const CONST_SYS_LAST = (Cell*)sys_dict + 4;

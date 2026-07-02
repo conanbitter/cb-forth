373f3d98a5db3fbb6b38d01e6b5bd79c49f535ae
Conan Bitter <conan.bitter@gmail.com>
2026-07-02 19:02:46 +0300

    Improve dictionary generation

--- codegen/dict.py
+++ codegen/dict.py
@@ -5,21 +5,20 @@ import struct
 DICT_NAME = "sys_dict"
 CELL_WIDTH = 4
 FLAG_IMMED = 0b10000000
-
-dict = []
-first = True
-latest = 0
-latest_prev = 0
+FLAG_COMPONLY = 0b01000000
 
 
-def encode_name(name, immed):
-    first_byte = len(name)
-    if immed:
-        first_byte |= FLAG_IMMED
+def encode_name(name, flags):
+    byte_flags = 0
+    if 'immed' in flags:
+        byte_flags |= FLAG_IMMED
+    if 'componly' in flags:
+        byte_flags |= FLAG_COMPONLY
+
+    first_byte = len(name) | byte_flags
     name_cells = ceil((len(name) + 2) / CELL_WIDTH)
-    last_byte = name_cells
-    if immed:
-        last_byte |= FLAG_IMMED
+    last_byte = name_cells | byte_flags
+
     name_sector = bytearray()
     name_sector.append(first_byte)
     name_sector.extend(name.encode("utf-8"))
@@ -30,28 +29,72 @@ def encode_name(name, immed):
     return [f"0x{i:0{CELL_WIDTH*2}X}" for i in cells]
 
 
-def get_prev():
-    global first, latest, latest_prev
-    result = ''
-    if first:
-        first = False
-        result = '0'
-        latest = 0
+class DictWord:
+    def __init__(self, name, code, flags):
+        self.address = 0
+        self.prev = 0
+        self.name = name
+        self.flagname = encode_name(name, flags)
+        self.code = code
+        self.data = []
+
+    def __len__(self):
+        return 2 + len(self.flagname) + len(self.data)
+
+
+latest = None
+current_pos = 0
+sysdict = []
+wordlist = {}
+
+
+def abs_address(offset):
+    return f"{DICT_NAME} + {offset}"
+
+
+def add_word(word):
+    global current_pos, latest
+    word.address = current_pos
+    if latest is None:
+        word.prev = None
     else:
-        result = f"{DICT_NAME} + {latest_prev}"
-    latest_prev = latest
-    return result
+        word.prev = latest.address
+    current_pos += len(word)
+    latest = word
+    sysdict.append(word)
+    wordlist[word.name] = word
 
 
-def add_native(name, code, immed):
-    global latest
-    data = []
-    data.append(get_prev())
-    data.extend(encode_name(name, False))
-    data.append(f"(Cell)&code_{code}")
-    dict.append(data)
-    latest += len(data)
-    # print("native", name, code, immed, data)
+def add_native(entry):
+    new_word = DictWord(entry['name'], entry['code'], entry.get('flags', []))
+    add_word(new_word)
+
+
+def add_const(word_entry):
+    new_word = DictWord(word_entry['name'], 'lit', [])
+    new_word.data.append(word_entry['const'])
+    add_word(new_word)
+
+
+def add_var(word_entry):
+    new_word = DictWord(word_entry['name'], 'lit', [])
+    new_word.data.append(f"(Cell)&{word_entry['var']}")
+    add_word(new_word)
+
+
+def get_wordcode(name):
+    word = wordlist[name]
+    return word.address + 1 + len(word.flagname)
+
+
+def add_forth(word_entry):
+    new_word = DictWord(word_entry['name'], 'docol', word_entry.get('flags', []))
+    for word in word_entry['body']:
+        if type(word) is str:
+            new_word.data.append(abs_address(get_wordcode(word)))
+        elif type(word) is int:
+            new_word.data.append(str(word))
+    add_word(new_word)
 
 
 words = []
@@ -61,20 +104,23 @@ with open("dictionary.json", 'r') as f:
     words = js['words']
 
 for entry in words:
+    proc_fun = None
     if entry['type'] == 'native':
-        if 'words' in entry:
-            for word in entry['words']:
-                add_native(word['name'], word['code'], word.get('immed', False))
-        elif 'name' in entry and 'code' in entry:
-            add_native(entry['name'], entry['code'], entry.get('immed', False))
+        proc_fun = add_native
+    elif entry['type'] == 'const':
+        proc_fun = add_const
+    elif entry['type'] == 'var':
+        proc_fun = add_var
+    elif entry['type'] == 'forth':
+        proc_fun = add_forth
+    else:
+        continue
 
-max_prev_len = 0
-for word in dict:
-    if len(word[0]) > max_prev_len:
-        max_prev_len = len(word[0])
+    if 'words' in entry:
+        for word in entry['words']:
+            proc_fun(word)
+    else:
+        proc_fun(entry)
 
-print(max_prev_len)
-
-for word in dict:
-    print(f"{word[0]}, {' '*(max_prev_len-len(word[0]))}", end='')
-    print(", ".join(word[1:]))
+for word in sysdict:
+    print(word.__dict__)
